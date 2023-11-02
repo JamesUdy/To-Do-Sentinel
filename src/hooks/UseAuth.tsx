5bbb5663208e15def651f1bde4a1f1c3afb3cd8f
JamesUdy <udayamvad@gmail.com>
2023-11-02 20:19:21 +0530

    feat: implement custom useAuth hook

---
+++ src/hooks/UseAuth.tsx
@@ -0,0 +1,19 @@
+import { auth } from "@/firebaseAuth/FirebaseAuth";
+import { useEffect, useState } from "react";
+import { User } from "firebase/auth";
+
+const useAuth = () => {
+    const [user, setUser] = useState<User | null>(null);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        auth.onAuthStateChanged((user) => {
+            setIsLoggedIn(user && user.uid ? true : false);
+            setUser(user);
+        });
+    });
+
+    return { user, isLoggedIn };
+};
+
+export default useAuth;
