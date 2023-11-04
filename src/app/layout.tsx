3609447dba4c22e300ecb5327ef04619cfbcbc6a
JamesUdy <udayamvad@gmail.com>
2023-11-04 11:23:03 +0530

    feat: implement dark mode functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import DarkThemeProvider from './provider/ThemeProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,9 +19,11 @@ export default function RootLayout({
 
     <html lang="en">
       <body className={`flex flex-col min-h-screen ${inter.className} w-full`}>
-        <main className='flex flex-1 w-full h-full'>
-          {children}
-        </main>
+        <DarkThemeProvider attribute='class' defaultTheme='system' enableSystem>
+          <main className='flex flex-1 w-full h-full'>
+            {children}
+          </main>
+        </DarkThemeProvider>
       </body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,11 @@
 import { auth } from "@/firebaseAuth/FirebaseAuth";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { useRouter } from "next/navigation";
+import { useTheme } from "next-themes";
 
 const Home = () => {
   const router = useRouter();  
+  const { theme, setTheme } = useTheme();
 
   const handleAuth = async () => {
     const googleSignIn = new GoogleAuthProvider();
@@ -32,9 +34,12 @@ const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center w-full">
       <span>To Do Sentinel</span>
-      <div>
-        <span>Light</span>
-      </div>
+      <button
+        className={`w-fit absolute right-5 top-2 p-2 rounded-md hover:scale-110 active:scale-100 duration-200 bg-slate-200 dark:bg-[#212933]`}
+        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      >
+        {theme === "light" ? "Dark" : "Light"}
+      </button>
       <button onClick={() => handleAuth()}>Google</button>
     </div>
   );
