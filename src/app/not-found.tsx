2ee1e51bfdf49eed8e96a208c64c52e254d24218
JamesUdy <udayamvad@gmail.com>
2023-11-05 21:43:04 +0530

    fix: no unescaped entities using &apos;

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -30,11 +30,11 @@ export default function NotFound() {
   return (
     <section className='flex flex-col items-center font-semibold justify-center max-w-full text-center space-y-2 sm:space-y-4 lg:space-y-6 container font-mono px-8'>
       <h2 className="text-xl sm:text-4xl font-bold text-red-950 dark:text-red-500">Oops! The Task Has Gone on an Adventure</h2>
-      <p className="text-sm sm:text-xl text-gray-950 dark:text-gray-600">The to-do task you're seeking seems to have embarked on an exciting journey, but we're here to assist you.</p>
+      <p className="text-sm sm:text-xl text-gray-950 dark:text-gray-600">The to-do task you&apos;re seeking seems to have embarked on an exciting journey, but we&apos;re here to assist you.</p>
       <div className="my-4">
         <Image src={errorImage} alt="Task not Found" className='w-48 sm:w-64 h-48 sm:h-64' />
       </div>
-      <p className="text-sm sm:text-xl text-blue-950 dark:text-blue-500">Fear not! We're redirecting you to the sign-in page in {countdown} seconds.</p>
+      <p className="text-sm sm:text-xl text-blue-950 dark:text-blue-500">Fear not! We&apos;re redirecting you to the sign-in page in {countdown} seconds.</p>
     </section>
   )
 };
