5dfaa3d45ed2f7194b3bcfc560117e107b516575
JamesUdy <udayamvad@gmail.com>
2023-11-05 21:35:29 +0530

    refactor: move hooks dir into app dir

--- src/app/todolist/page.tsx
+++ src/app/todolist/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import useAuth from '../../hooks/useAuth';
+import useAuth from "@/app/hooks/useAuth";
 import { auth } from '@/firebaseAuth/FirebaseAuth';
 import { signOut } from 'firebase/auth';
 import { useRouter } from 'next/navigation';
