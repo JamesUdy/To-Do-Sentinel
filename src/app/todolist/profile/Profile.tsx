b755467e93cf58a6f5a177ff4b407993ebec8970
JamesUdy <udayamvad@gmail.com>
2023-11-07 22:05:35 +0530

    ui: use headless ui to implement transform in profile component

--- src/app/todolist/profile/Profile.tsx
+++ src/app/todolist/profile/Profile.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
+import { Transition } from '@headlessui/react';
 
 interface ProfileProps {
   showProfile: boolean;
@@ -10,13 +11,24 @@ interface ProfileProps {
 
 const Profile: React.FC<ProfileProps> = ({showProfile, userName, userDp, handleLogout}) => {
   return (
-    <section className={`${showProfile ? 'flex' : 'hidden'}`}>
+    <Transition 
+      show={showProfile}
+      enter="transition ease-in-out duration-300 transform"
+      enterFrom="-translate-x-full"
+      enterTo="translate-x-0"
+      leave="transition ease-in-out duration-300 transform"
+      leaveFrom="translate-x-0"
+      leaveTo="-translate-x-full"
+      className='absolute top-0 left-0 bg-white max-h-full'
+    >
+          <section className=''>
         <div>
           {userDp && <Image src={userDp} alt='User Dp' width={100} height={100} className='rounded-full' priority={true} />}
         </div>
         {userName && <div>{userName}</div>}
         <button onClick={() => handleLogout()}>Logout</button>
-    </section>
+        </section>
+      </Transition>
   );
 };
 
