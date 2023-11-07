b60140c361f59ba413ccca422991d2e5d8481ab0
JamesUdy <udayamvad@gmail.com>
2023-11-07 09:12:13 +0530

    ui: made sign in component responsive

--- src/app/signIn/SignIn.tsx
+++ src/app/signIn/SignIn.tsx
@@ -43,8 +43,8 @@ const SignIn = () => {
 
   return (
     <section className="flex flex-col items-center my-auto w-full space-y-4 font-mono">
-      <span className="text-6xl font-bold website-name">To Do Sentinel</span>
-      <p className="text-lg text-center font-medium text-slate-800 dark:text-slate-500 my-4 w-1/2">
+      <span className="text-5xl sm:text-6xl font-bold website-name">To Do Sentinel</span>
+      <p className="text-md sm:text-lg text-center font-medium text-slate-800 dark:text-slate-500 my-4 px-4 sm:px-0 lg:w-1/2">
         Welcome to To Do Sentinel - Your Ultimate Task Management App! 🚀✨<br />
         Stay organized, boost productivity, and never forget a task 📈✅.
         <br /><br />
@@ -53,14 +53,14 @@ const SignIn = () => {
         Sign in now to start exploring your to-do list and supercharge your productivity! 🚀💪
       </p>
       <section className="overflow-hidden w-full mx-auto flex justify-center" aria-disabled='true'>
-        <Image loading="lazy" src={lightThemeLogo} alt="ToDo Sentinel Logo" className="rotate-logo w-1/4 inline-block dark:hidden"/>
-        <Image loading="lazy" src={darkThemeLogo} alt="ToDo Sentinel Logo" className="rotate-logo w-1/4 hidden dark:inline-block"/>
+        <Image loading="lazy" src={lightThemeLogo} alt="ToDo Sentinel Logo" className="rotate-logo w-1/2 sm:w-1/4 inline-block dark:hidden"/>
+        <Image loading="lazy" src={darkThemeLogo} alt="ToDo Sentinel Logo" className="rotate-logo w-1/2 sm:w-1/4 hidden dark:inline-block"/>
       </section>
       <section className="relative w-1/2 flex justify-center">
         <button className="px-4 py-1 rounded-md font-bold ease-in duration-200 text-slate-200 dark:text-slate-950 bg-slate-800 hover:bg-slate-950 dark:bg-slate-100 hover:dark:bg-slate-200" onClick={() => handleAuth()}>Sign In</button>
-      <section className="absolute left-1/4 bottom-1/3">
-        <Image loading="lazy" src={DarkCurvedArrow} alt="Sign In Button Indicating Arrow" className="w-24 mx-4 inline-block dark:hidden" />
-        <Image loading="lazy" src={LightCurvedArrow} alt="Sign In Button Indicating Arrow" className="w-24 mx-4 hidden dark:inline-block" />
+      <section className="absolute -left-12 sm:left-12 lg:left-24 xl:left-1/4 bottom-1/3 flex justify-start">
+        <Image loading="lazy" src={DarkCurvedArrow} alt="Sign In Button Indicating Arrow" className="w-16 lg:w-20 xl:w-24 mx-4 inline-block dark:hidden" />
+        <Image loading="lazy" src={LightCurvedArrow} alt="Sign In Button Indicating Arrow" className="w-16 lg:w-20 xl:w-24 mx-4 hidden dark:inline-block" />
       </section>
       </section>
        {/* <a href="https://www.flaticon.com/free-icons/curved-arrow" title="curved arrow icons">Curved arrow icons created by Freepik - Flaticon</a>*/}
