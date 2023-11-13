98e6ab96b6e9df59c587a58bc2feb256759d475e
JamesUdy <udayamvad@gmail.com>
2023-11-13 21:56:50 +0530

    feat: show user name and email address

--- src/app/user-details/page.tsx
+++ src/app/user-details/page.tsx
@@ -13,23 +13,37 @@ const UserDetails = () => {
 
   const userDp = user?.photoURL;
   const userName = user?.displayName;
+  const userEmail = user?.email;
 
   return (
     <section className='container flex items-center justify-center'>
-      <div className='flex items-center bg-slate-400/80 dark:bg-slate-900 py-12 px-8 rounded-xl'>
+      <div className='flex items-center space-x-6 bg-slate-400/80 dark:bg-slate-900 py-12 px-8 rounded-xl'>
         <div>
           {userDp && <Image src={userDp} alt='User Dp' width={100} height={100} className='rounded-md' priority={true} />}
         </div>
         <div>
+          <section className='flex flex-col'>
+            <span className='text-xl tracking-tight font-bold text-slate-950 dark:text-slate-300'>{userName}</span>
+            <span className='text-md tracking-tighter font-medium text-slate-800 dark:text-slate-500'>{userEmail}</span>
+          </section>
+          {/* <section className='flex items-center'>
+            <div>
+              <ProfileName/>
+            </div>
+            <div className='flex flex-col'>
+              <span className='text-slate-700 dark:text-slate-500 text-sm font-medium'>name</span>
+              <span className='text-xl'>{userName}</span>
+            </div>
+          </section>
           <section className='flex items-center'>
             <div>
               <ProfileName/>
             </div>
             <div className='flex flex-col'>
-              <span>Name</span>
-              <span>{userName}</span>
+              <span className='text-slate-700 dark:text-slate-500 text-sm font-medium'>email</span>
+              <span className='text-xl'>{userEmail}</span>
             </div>
-          </section>
+          </section> */}
         </div>
       </div>
       <ThemeToggleButton/>
