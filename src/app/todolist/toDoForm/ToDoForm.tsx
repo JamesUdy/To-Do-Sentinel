a42de23535d965b411600a6ca40c014c2114d2ef
JamesUdy <udayamvad@gmail.com>
2023-11-16 21:54:22 +0530

    ui: style to do form header

--- src/app/todolist/toDoForm/ToDoForm.tsx
+++ src/app/todolist/toDoForm/ToDoForm.tsx
@@ -3,7 +3,8 @@ import ToDoCategory from './toDoCategory/ToDoCategory';
 
 const ToDoForm = () => {
   return (
-    <section className='container w-full flex justify-center font-medium'>
+    <section className='container w-full flex flex-col items-center mt-6 space-y-4 font-medium todo-form'>
+      <span className='text-xl font-semibold text-slate-700 dark:text-slate-400'>🎯 Add a Quest</span>
       <div className='w-1/4 flex flex-col items-center space-y-4'>
         <input className='w-full py-2 px-3 text-sm bg-slate-300 dark:bg-slate-900 text-slate-700 dark:text-slate-400 caret-slate-700 dark:caret-slate-400 placeholder:text-slate-600 dark:placeholder:text-slate-600 placeholder:text-sm rounded-md outline outline-1 outline-offset-2 outline-slate-400 dark:outline-slate-900 focus:outline focus:outline-2' placeholder='Title' type='text' />
         <textarea placeholder='Describe your task' className='w-full h-32 max-h-44 min-h-24 py-2 px-3 text-sm bg-slate-300 dark:bg-slate-900 text-slate-700 dark:text-slate-400 caret-slate-700 dark:caret-slate-400 placeholder:text-slate-600 dark:placeholder:text-slate-600 placeholder:text-sm rounded-md outline outline-1 outline-offset-2 outline-slate-400 dark:outline-slate-900 focus:outline focus:outline-2'></textarea>
