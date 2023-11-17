8f34800cd6bb9509cb1f7f349575635f9ef7d444
JamesUdy <udayamvad@gmail.com>
2023-11-17 17:57:58 +0530

    refactor: update toDo api

--- src/api/toDo.tsx
+++ src/api/toDo.tsx
@@ -3,27 +3,31 @@ import { collection, addDoc, updateDoc, doc, deleteDoc } from "firebase/firestor
 
 interface AddToDoProps {
     userId: string;
-    task: string;
-    description: string;
-    status: string;
+    taskTitle: string;
+    taskDescription: string;
+    taskPriority: string;
+    taskProgress: string;
+    taskDueDate: string;
 };
 
 interface ToggleToDoStatusProps {
     docId: string;
-    status: string;
+    taskProgress: string;
 };
 
 interface DeleteToDoProps {
     docId: string;
 };
 
-const addToDo = async ({userId, task, description, status }: AddToDoProps) => {
+const addToDo = async ({userId, taskTitle, taskDescription, taskPriority, taskProgress, taskDueDate }: AddToDoProps) => {
     try {
         await addDoc(collection(db, 'toDo'), {
             userId: userId,
-            task: task,
-            description: description,
-            status: status,
+            taskTitle: taskTitle,
+            taskDescription: taskDescription,
+            taskPriority: taskPriority,
+            taskProgress: taskProgress,
+            taskDueDate: taskDueDate,
             createdAt: new Date().getTime(),
         });
     } catch (err) {
@@ -31,11 +35,11 @@ const addToDo = async ({userId, task, description, status }: AddToDoProps) => {
     };
 };
 
-const toggleToDoStatus = async ({docId, status}: ToggleToDoStatusProps) => {
+const toggleToDoStatus = async ({docId, taskProgress}: ToggleToDoStatusProps) => {
     try {
         const todoRef = doc(db, "toDo", docId);
         await updateDoc(todoRef, {
-            status,
+            taskProgress,
         });
     } catch (err) {
         console.log(err);
