aac5319bd737ccc4c885a7dfdcaabe12e9378dbc
JamesUdy <udayamvad@gmail.com>
2023-11-20 18:55:04 +0530

    ui: style scroll bar for both themes

--- src/app/todolist/page.tsx
+++ src/app/todolist/page.tsx
@@ -6,11 +6,14 @@ import { auth } from '@/firebaseAuth/FirebaseAuth';
 import { signOut } from 'firebase/auth';
 import { useRouter } from 'next/navigation';
 import { Navbar, TaskList, ToDoForm, SearchTask, Profile, Loader } from './todoAssets';
+import './index.css';
+import { useTheme } from 'next-themes';
 
 const ToDoList = () => {
   const router = useRouter();
   const {user, setUser} = useAuth();
   const [showProfile, setShowProfile] = useState(false);
+  const { theme } = useTheme();
 
   const handleShowProfile = () => {
     setShowProfile(!showProfile);
@@ -22,12 +25,14 @@ const ToDoList = () => {
     router.push('/');
   };
 
+  const scrollBar = theme === 'dark' ? 'to-do-list-dark' : 'to-do-list-light';
+
   return (
-    <section className='relative flex flex-col w-full py-2 px-4 sm:px-0 max-h-screen overflow-y-hidden'>
+    <section className='relative flex flex-col w-full py-2 px-4 sm:px-1 max-h-screen overflow-y-hidden'>
       {user ? (
         <>
           <Navbar handleShowProfile={handleShowProfile} userDp={user.photoURL}/>
-          <section className='overflow-y-scroll'>
+          <section className={`overflow-y-scroll ${scrollBar}`}>
             <ToDoForm/>
             <SearchTask/>
             <TaskList/>
