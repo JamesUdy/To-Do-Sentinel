abd0f59bfd750487cd075798999260952f16e8fb
JamesUdy <udayamvad@gmail.com>
2023-11-20 12:41:40 +0530

    ui: style task priority component

--- src/app/todolist/taskList/taskCard/TaskCard.tsx
+++ src/app/todolist/taskList/taskCard/TaskCard.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { ListProps } from '../../fetchToDoData/FetchToDoData';
+import { Delete, Edit } from '@/assets';
+import { categories } from '../../toDoForm/toDoCategory/Categories';
+import { useTheme } from 'next-themes';
 
 const formatDate = (dateString: string): string => {
     const date = new Date(dateString);
@@ -12,13 +15,26 @@ const formatDate = (dateString: string): string => {
 
 const TaskCard: React.FC<{task: ListProps}> = ({task}) => {
     const formattedDate = formatDate(task.createdAt);
+    const category = categories.find(category => category.label === task.taskPriority);
+    const { theme } = useTheme();
+
+    const backgroundColor = theme === 'dark' ? category?.colors.dark : category?.colors.light;
 
   return (
-    <section className='w-full bg-white dark:bg-slate-900 p-2 flex flex-col justify-between h-40 space-y-2 rounded-lg shadow-lg shadow-slate-600 dark:shadow-black' key={task.id}>
-        <div className='p-2 flex flex-col space-y-1'>
-            <span className='text-lg font-semibold'>{task.taskTitle}</span>
-            <span className='text-sm text-start font-normal'>{task.taskDescription}</span>
-            {/* <span>{task.taskDueDate}</span> */}
+    <section className='w-full bg-white dark:bg-slate-900 p-2 flex flex-col justify-between h-48 space-y-2 rounded-lg shadow-lg shadow-slate-600 dark:shadow-black' key={task.id}>
+        <div className='flex flex-col space-y-2'>
+            <div className='flex justify-between items-center py-2'>
+                <span className={`${backgroundColor} ml-1 px-2 rounded-md text-sm text-white`}>{task.taskPriority}</span>
+                <div className='flex space-x-2'>
+                    <Edit/>
+                    <Delete/>
+                </div>
+            </div>
+            <div className='px-2 flex flex-col space-y-1'>
+                <span className='text-lg font-semibold'>{task.taskTitle}</span>
+                <span className='text-sm text-start font-normal'>{task.taskDescription}</span>
+                {/* <span>{task.taskDueDate}</span> */}
+            </div>
         </div>
         <span className='text-end w-full text-2xs text-slate-500'>{formattedDate}</span>
     </section>
@@ -26,3 +42,7 @@ const TaskCard: React.FC<{task: ListProps}> = ({task}) => {
 };
 
 export default TaskCard;
+function useThemeProps() {
+    throw new Error('Function not implemented.');
+}
+
--- src/app/todolist/toDoForm/toDoCategory/Categories.ts
+++ src/app/todolist/toDoForm/toDoCategory/Categories.ts
@@ -1,31 +1,63 @@
+interface ColorProps {
+    light: string;
+    dark: string;
+};
+
 interface categoriesProps {
     id: number;
     label: string;
+    colors: ColorProps;
 };
 
-export const categories: categoriesProps[] = [
-    {
-        id: 1,
-        label: 'Personal',
-    },
-    {
-        id: 2,
-        label: 'Home',
-    },
-    {
-        id: 3,
-        label: 'Studies',
-    },
-    {
-        id: 4,
-        label: 'Business',
-    },
-    {
-        id: 5,
-        label: 'Fitness',
-    },
-    {
-        id: 6,
-        label: 'Other',
-    },
-];
+export const categories: categoriesProps[] =// categories.json
+[
+  {
+    "id": 1,
+    "label": "Personal",
+    "colors": {
+      "light": "bg-[#00b4d8]",  // Light Blue
+      "dark": "bg-[#0077b6]"    // Dark Blue
+    }
+  },
+  {
+    "id": 2,
+    "label": "Home",
+    "colors": {
+      "light": "bg-[#2ecc71]",  // Light Green
+      "dark": "bg-[#006400]"    // Dark Green
+    }
+  },
+  {
+    "id": 3,
+    "label": "Studies",
+    "colors": {
+      "light": "bg-[#f48c06]",  // Light Orange
+      "dark": "bg-[#dc2f02]"    // Dark Orange
+    }
+  },
+  {
+    "id": 4,
+    "label": "Business",
+    "colors": {
+      "light": "bg-[#10002b]",  // Light Dark Blue
+      "dark": "bg-[#6200b3]"    // Dark Violet
+    }
+  },
+  {
+    "id": 5,
+    "label": "Fitness",
+    "colors": {
+      "light": "bg-[#e74c3c]",  // Light Red
+      "dark": "bg-[#d90429]"    // Dark Red
+    }
+  },
+  {
+    "id": 6,
+    "label": "Other",
+    "colors": {
+      "light": "bg-[#212529]",  // Light Gray
+      "dark": "bg-[#495057]"    // Dark Gray
+    }
+  }
+]
+
