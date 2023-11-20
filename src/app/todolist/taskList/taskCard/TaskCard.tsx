5e553d2f86909206ab146ccd0ac8d9caf1d03df4
JamesUdy <udayamvad@gmail.com>
2023-11-20 19:18:13 +0530

    feat: implement task deletion when click delete button

--- src/app/todolist/taskList/taskCard/TaskCard.tsx
+++ src/app/todolist/taskList/taskCard/TaskCard.tsx
@@ -3,6 +3,8 @@ import { ListProps } from '../../fetchToDoData/FetchToDoData';
 import { Delete, Edit } from '@/assets';
 import { categories } from '../../toDoForm/toDoCategory/Categories';
 import { useTheme } from 'next-themes';
+import { deleteToDo } from '@/api/toDo';
+import toast from 'react-hot-toast';
 
 const formatDate = (dateString: string): string => {
     const date = new Date(dateString);
@@ -14,11 +16,21 @@ const formatDate = (dateString: string): string => {
 };
 
 const TaskCard: React.FC<{task: ListProps}> = ({task}) => {
-    const formattedDate = formatDate(task.createdAt);
-    const category = categories.find(category => category.label === task.taskPriority);
-    const { theme } = useTheme();
+  const formattedDate = formatDate(task.createdAt);
+  const { theme } = useTheme();
 
-    const backgroundColor = theme === 'dark' ? category?.colors.dark : category?.colors.light;
+  const category = categories.find(category => category.label === task.taskPriority);
+  const backgroundColor = theme === 'dark' ? category?.colors.dark : category?.colors.light;
+
+  const handleDeleteTask = (taskId: string) => {
+    const isConfirmed = window.confirm("❗️ Are you sure you want to permanently delete this task? This action cannot be undone.");
+    if(isConfirmed) {
+        deleteToDo({ docId: taskId });
+        toast.success('Task successfully deleted!');
+    } else {
+        toast.error('Task deletion cancelled.');
+    };
+  };
 
   return (
     <section className='w-full bg-white dark:bg-slate-900 p-2 flex flex-col justify-between sm:h-40 xlm:h-48 space-y-2 rounded-lg shadow-lg shadow-slate-600 dark:shadow-black' key={task.id}>
@@ -26,8 +38,12 @@ const TaskCard: React.FC<{task: ListProps}> = ({task}) => {
             <div className='flex justify-between items-center py-2'>
                 <span className={`${backgroundColor} ml-1 px-2 rounded-md text-sm text-white shadow-md shadow-slate-700 dark:shadow-slate-950`}>{task.taskPriority}</span>
                 <div className='flex space-x-2'>
-                    <Edit/>
-                    <Delete/>
+                    <span>
+                        <Edit/>
+                    </span>
+                    <span className='cursor-pointer' onClick={() => handleDeleteTask(task.id)}>
+                        <Delete/>
+                    </span>
                 </div>
             </div>
             <div className='px-2 flex flex-col space-y-1'>
