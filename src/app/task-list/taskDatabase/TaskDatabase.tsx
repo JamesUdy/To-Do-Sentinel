792509c7d373e5fe59ac0eaf826c91aa601d3e23
JamesUdy <udayamvad@gmail.com>
2023-11-22 21:08:41 +0530

    ui: change font style in task repo page

--- src/app/task-list/taskDatabase/TaskDatabase.tsx
+++ src/app/task-list/taskDatabase/TaskDatabase.tsx
@@ -40,14 +40,14 @@ const TaskDatabase = () => {
   const scrollBar = theme === 'dark' ? 'to-do-list-dark' : 'to-do-list-light';
 
   return (
-    <section className='relative flex flex-col w-full py-2 px-4 sm:px-1 max-h-screen overflow-y-hidden'>
+    <section className='relative flex flex-col w-full py-2 px-4 sm:px-1 max-h-screen overflow-y-hidden todo-list'>
       {user ? (
         <>
           <Navbar handleShowProfile={handleShowProfile} userDp={user.photoURL}/>
-          <span className='text-md w360:text-lg sm:text-xl font-semibold todo-list w-full text-center'>🗂️ Task Repository</span>
+          <span className='text-md w360:text-lg sm:text-xl font-semibold w-full text-center'>🗂️ Task Repository</span>
           <SearchTask keyword={keyword} handleKeywordChanges={handleKeywordChanges} clearKeyword={clearKeyword} />
           <section className={`overflow-y-scroll ${scrollBar}`}>
-            <div className='w-full px-2 container flex flex-col items-center my-4 todo-list'>
+            <div className='w-full px-2 container flex flex-col items-center my-4'>
                 {toDoListData && (
                     <section className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 sm:w-2/3 xl:w-1/2 gap-6'>
                         {toDoListData.map((task) => (
