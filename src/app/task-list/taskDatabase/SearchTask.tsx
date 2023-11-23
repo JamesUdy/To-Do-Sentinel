fe0950f3d68dc99eace79974908710ba82e99cdb
JamesUdy <udayamvad@gmail.com>
2023-11-23 23:16:32 +0530

    ui: style search input component

--- src/app/task-list/taskDatabase/SearchTask.tsx
+++ src/app/task-list/taskDatabase/SearchTask.tsx
@@ -31,16 +31,16 @@ const SearchTask: React.FC<SearchTaskProps> = ({keyword, handleKeywordChanges, c
 
   return (
     <section className='w-1/2 flex mx-auto my-6'>
-      <div className="relative w-full mt-2 flex items-center shadow-lg shadow-neutral-900">
-        <div className='absolute left-0'>
+      <div className="relative w-full mt-2 rounded-md flex items-center shadow-lg shadow-slate-600 dark:shadow-black">
+        <div className='absolute left-4'>
           <Search/>
         </div>
         <input
           type="text"
           name="search"
           id="search"
-          placeholder={'Search recent projects'}
-          className="block w-full text-sm rounded-sm border-0 py-1.5 pr-12 text-neutral-300 shadow-sm ring-1 ring-inset ring-neutral-700 placeholder:text-neutral-600 focus:ring-1 focus:ring-inset focus:ring-neutral-700 bg-neutral-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-1 focus-visible:outline-blue-600"
+          placeholder={'Search tasks'}
+          className="block pl-12 w-full text-sm rounded-md border-0 py-1.5 pr-12 dark:text-neutral-300 shadow-sm ring-1 ring-inset ring-slate-400 dark:ring-slate-900 placeholder:text-neutral-600 focus:ring-1 focus:ring-inset bg-white dark:bg-slate-900 focus-visible:outline-2 focus-visible:outline focus-visible:outline-blue-600"
           value={keyword}
           onChange={handleKeywordChanges}
           ref={searchInputRef}
@@ -56,8 +56,8 @@ const SearchTask: React.FC<SearchTaskProps> = ({keyword, handleKeywordChanges, c
 function ClearKeywordButton() {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
-      className="w-5 h-5 stroke-neutral-500 cursor-pointer"
+      xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke='currentColor' strokeWidth={1.5}
+      className="w-5 h-5 cursor-pointer"
     >
       <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"/>
     </svg>
