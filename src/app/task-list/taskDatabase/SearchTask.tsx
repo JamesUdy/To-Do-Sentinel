a34080eef55fb68fb4377c53af9cb07e99c8f948
JamesUdy <udayamvad@gmail.com>
2023-11-23 23:35:21 +0530

    ui: make search input component responsive

--- src/app/task-list/taskDatabase/SearchTask.tsx
+++ src/app/task-list/taskDatabase/SearchTask.tsx
@@ -30,7 +30,7 @@ const SearchTask: React.FC<SearchTaskProps> = ({keyword, handleKeywordChanges, c
   }, []);
 
   return (
-    <section className='w-1/2 flex mx-auto my-6'>
+    <section className='w-full px-4 sm:px-0 sm:w-2/3 lg:w-1/2 flex mx-auto my-6'>
       <div className="relative w-full mt-2 rounded-md flex items-center shadow-lg shadow-slate-600 dark:shadow-black">
         <div className='absolute left-4'>
           <Search/>
@@ -39,8 +39,8 @@ const SearchTask: React.FC<SearchTaskProps> = ({keyword, handleKeywordChanges, c
           type="text"
           name="search"
           id="search"
-          placeholder={'Search tasks'}
-          className="block pl-12 w-full text-sm rounded-md border-0 py-1.5 pr-12 dark:text-neutral-300 shadow-sm ring-1 ring-inset ring-slate-400 dark:ring-slate-900 placeholder:text-neutral-600 focus:ring-1 focus:ring-inset bg-white dark:bg-slate-900 focus-visible:outline-2 focus-visible:outline focus-visible:outline-blue-600"
+          placeholder={'Find a specific task title...'}
+          className="block pl-10 sm:pl-12 w-full text-sm rounded-md border-0 py-1.5 pr-12 dark:text-neutral-300 shadow-sm ring-1 ring-inset ring-slate-400 dark:ring-slate-900 placeholder:text-neutral-600 focus:ring-1 focus:ring-inset bg-white dark:bg-slate-900 focus-visible:outline-2 focus-visible:outline focus-visible:outline-blue-600"
           value={keyword}
           onChange={handleKeywordChanges}
           ref={searchInputRef}
