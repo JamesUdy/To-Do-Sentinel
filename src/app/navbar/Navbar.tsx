a850bd8715159c143ee4080490a4e6cfffa9036a
JamesUdy <udayamvad@gmail.com>
2023-11-29 09:09:30 +0530

    feat: add page links to navbar

--- src/app/navbar/Navbar.tsx
+++ src/app/navbar/Navbar.tsx
@@ -1,6 +1,8 @@
 import { darkThemeLogo, lightThemeLogo } from '@/assets';
 import Image from "next/image";
+import Link from 'next/link';
 import React from 'react';
+import { usePathname } from 'next/navigation';
 
 interface NavbarProps {
   handleShowProfile: () => void;
@@ -8,6 +10,22 @@ interface NavbarProps {
 };
 
 const Navbar: React.FC<NavbarProps> = ({handleShowProfile, userDp}) => {
+  const links = [
+    { 
+      href: '/', 
+      label: 'Home' 
+    },
+    { 
+      href: '/todo-list', 
+      label: 'Form' 
+    },
+    { 
+      href: '/task-repo', 
+      label: 'Repositories' 
+    },
+  ];
+
+  const pathName = usePathname();
 
   return (
     <section className='flex w-full justify-between items-center px-4'>
@@ -18,8 +36,19 @@ const Navbar: React.FC<NavbarProps> = ({handleShowProfile, userDp}) => {
         </div>
         <span className="text-xl w360:text-2xl sm:text-2xl xl:text-3xl font-bold website-name">To Do Sentinel</span>
       </section>
-      <div onClick={() => handleShowProfile()}>
-        {userDp && <Image src={userDp} alt='User Dp' width={40} height={40} className='rounded-full cursor-pointer' priority={true} />}
+      <div className='flex items-center space-x-12'>
+        <section className='text-sm font-bold space-x-4 profile-page-font'>
+          {links.map((link) => (
+            <Link key={link.href} href={link.href}>
+              <span className={`ease-in duration-200 ${pathName === link.href ? 'underline underline-offset-2' : 'hover:underline hover:underline-offset-2'}`}>
+                {link.label}
+              </span>
+            </Link>
+          ))}
+        </section>
+        <div onClick={() => handleShowProfile()}>
+          {userDp && <Image src={userDp} alt='User Dp' width={40} height={40} className='rounded-full cursor-pointer' priority={true} />}
+        </div>
       </div>
     </section>
   );
