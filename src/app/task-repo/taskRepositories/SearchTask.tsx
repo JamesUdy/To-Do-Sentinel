0b5bc81d481fb5c0f7ea5e31906fa4023652cd0b
JamesUdy <udayamvad@gmail.com>
2023-12-03 12:32:43 +0530

    ui: change placeholder text color

--- src/app/task-repo/taskRepositories/SearchTask.tsx
+++ src/app/task-repo/taskRepositories/SearchTask.tsx
@@ -40,7 +40,7 @@ const SearchTask: React.FC<SearchTaskProps> = ({keyword, handleKeywordChanges, c
           name="search"
           id="search"
           placeholder={'Find a specific task title...'}
-          className="block pl-10 sm:pl-12 w-full text-sm rounded-md border-0 py-1.5 pr-12 dark:text-neutral-300 shadow-sm ring-1 ring-inset ring-slate-400 dark:ring-slate-900 placeholder:text-neutral-600 bg-white dark:bg-slate-900 focus:ring-1 focus:ring-inset hover:outline-2 hover:outline hover:outline-blue-600 focus-visible:outline-2 focus-visible:outline focus-visible:outline-blue-600"
+          className="block pl-10 sm:pl-12 w-full text-sm rounded-md border-0 py-1.5 pr-12 dark:text-slate-300 shadow-sm ring-1 ring-inset ring-slate-400 dark:ring-slate-900 placeholder:text-slate-600 bg-white dark:bg-slate-900 focus:ring-1 focus:ring-inset hover:outline-2 hover:outline hover:outline-blue-600 focus-visible:outline-2 focus-visible:outline focus-visible:outline-blue-600"
           value={keyword}
           onChange={handleKeywordChanges}
           ref={searchInputRef}
@@ -49,7 +49,7 @@ const SearchTask: React.FC<SearchTaskProps> = ({keyword, handleKeywordChanges, c
           {keyword.length > 0 ? (
             <ClearKeywordTask/>
           ) : (
-            <span className='text-xs pt-1 pr-2 text-neutral-600'>CTRL + F</span>
+            <span className='text-xs pt-1 pr-2 text-slate-600'>CTRL + F</span>
           )}
         </div>
       </div>
