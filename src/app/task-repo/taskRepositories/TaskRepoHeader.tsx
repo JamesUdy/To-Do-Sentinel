36275f54bc8aca78b2953463253d2d1837d2248f
JamesUdy <udayamvad@gmail.com>
2023-12-09 22:00:56 +0530

    ui: make instructions component overflow scroll

--- src/app/task-repo/taskRepositories/TaskRepoHeader.tsx
+++ src/app/task-repo/taskRepositories/TaskRepoHeader.tsx
@@ -1,14 +1,18 @@
 import { Dialog, Transition } from '@headlessui/react';
 import { Fragment, useState } from 'react';
 import { Delete, Edit, Info, LeftArrow } from '@/assets';
+import { useTheme } from 'next-themes';
 
 const TaskRepoHeader = () => {
   const [isPopoverOpen, setIsPopoverOpen] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
+  const { theme } = useTheme();
 
   const handleClose = () => {
     setIsOpen(false);
   };
+
+  const scrollBar = theme === 'dark' ? 'to-do-list-dark' : 'to-do-list-light';
 
   return (
     <div className='text-md w360:text-lg sm:text-xl font-semibold text-center flex items-center justify-center space-x-4'>
@@ -43,19 +47,18 @@ const TaskRepoHeader = () => {
                   leaveFrom="opacity-100 scale-100"
                   leaveTo="opacity-0 scale-95"
                 >
-                  <Dialog.Panel className="w-full border-2 text-slate-700 dark:text-slate-400 border-slate-500/50 dark:border-slate-600/10 max-w-md transform overflow-hidden rounded-2xl bg-white dark:bg-slate-950 py-6 px-8 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Panel className="w-full border-2 text-slate-700 dark:text-slate-400 border-slate-500/50 dark:border-slate-600/10 max-w-md transform rounded-2xl bg-white dark:bg-slate-950 py-6 px-8 text-left align-middle shadow-xl transition-all space-y-4">
                     <Dialog.Title
                       as="h3"
-                      className="text-lg font-medium leading-6"
+                      className="text-xl font-bold w-full"
                     >
-                      Payment successful
+                      User Guide
                     </Dialog.Title>
-                    <div>
-                      <h2></h2>
-                      <ol className='list-decimal'>
+                    <div className='relative h-72 w360:h-96 overflow-hidden'>
+                      <ol className={`overflow-y-scroll h-full list-decimal space-y-4 ${scrollBar}`}>
                         <li>
-                          <strong>Orange, Green, and Grey Balls:</strong>
-                          <ul className='flex flex-col space-y-2 py-2'>
+                          1. <strong>Orange, Green, and Grey Balls:</strong>
+                          <ul className='flex flex-col space-y-2 py-2 px-2 text-sm'>
                             <li className='flex space-x-2 items-center'>
                               <span className='bg-gray-600 dark:bg-gray-300 animate-bounce w-4 h-4 px-2 rounded-full'></span>
                               <span> - Represents tasks not yet started.</span>  
@@ -74,32 +77,32 @@ const TaskRepoHeader = () => {
                         </li>
 
                         <li>
-                          <strong>Edit and Delete Buttons:</strong>
-                          <ul className='flex flex-col space-y-2 py-2'>
+                          2. <strong>Edit and Delete Buttons:</strong>
+                          <ul className='flex flex-col space-y-2 py-2 px-2 text-sm'>
                             <li className='flex space-x-2 items-center'><Edit/><span> - Allows you to update the task.</span></li>
                             <li className='flex space-x-2 items-center'><Delete/><span> - Enables task deletion.</span></li>
                           </ul>
                         </li>
 
                         <li>
-                          <strong>Span with Category Names:</strong>
-                          <ul className='flex flex-col space-y-2 py-2'>
-                            <li><span className='bg-slate-950 dark:bg-white py-1 px-2 rounded-md font-semibold text-sm text-white dark:text-slate-950 shadow-md shadow-slate-700 dark:shadow-slate-950'>Category</span>
+                          3. <strong>Span with Category Names:</strong>
+                          <ul className='flex flex-col space-y-2 py-2 px-2 text-sm'>
+                            <li><span className='bg-slate-950 dark:bg-white py-1 px-2 rounded-md font-semibold text-xs text-white dark:text-slate-950 shadow-md shadow-slate-700 dark:shadow-slate-950'>Category</span>
                             <span> -  Denotes the category of tasks.</span></li>
                           </ul>
                         </li>
 
                         <li>
-                          <strong>Ctrl + F or Cmd + F:</strong>
-                          <ul>
+                          4. <strong>Ctrl + F or Cmd + F:</strong>
+                          <ul className='flex flex-col space-y-2 py-2 px-2 text-sm'>
                             <li><em>Ctrl + F or Cmd + F:</em> Use these keyboard shortcuts to focus on the search bar.</li>
                           </ul>
                         </li>
 
                         <li>
-                          <strong>Date Format:</strong>
-                          <ul>
-                            <li><em>__-__-____ Date Format:</em> Displays the task creation date.</li>
+                          5. <strong>Date Format:</strong>
+                          <ul className='flex flex-col space-y-2 py-2 px-2 text-sm'>
+                            <li><em>DD-MM-YYYY Date Format:</em> Displays the task creation date.</li>
                           </ul>
                         </li>
                       </ol>
