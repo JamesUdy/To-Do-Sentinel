4a80ed2b36411fd7877a2f09b4e68160c4cce0b2
JamesUdy <udayamvad@gmail.com>
2023-12-10 22:30:55 +0530

    ui: adjust size of dark mode inner svg

--- src/app/profile/SwitchButton.tsx
+++ src/app/profile/SwitchButton.tsx
@@ -16,14 +16,14 @@ const SwitchButton: React.FC<SwitchButtonProps> = ({isDarkTheme, handleDarkTheme
               onChange={() => handleDarkThemeChange()}
               className={`${isDarkTheme ? "bg-slate-50" : "bg-slate-950"} relative justify-between items-center inline-flex h-7 w-16 shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out outline-none`}
             >
-                <span className='scale-125'>🌜</span>
+                <span className='scale-110'>🌜</span>
                 <span className="sr-only">Use setting</span>
                 <span
                   aria-hidden="true"
                     className={`${isDarkTheme ? 'translate-x-9 bg-slate-950' : 'translate-x-0 bg-white'} absolute z-10 top-0
                       pointer-events-none inline-block h-6 w-6 transform rounded-full shadow-lg ring-0 transition duration-500 ease-in-out`}
               />
-              <span className='scale-125'>🌞</span>
+              <span className='scale-110'>🌞</span>
             </Switch>
         </section>
     </>
