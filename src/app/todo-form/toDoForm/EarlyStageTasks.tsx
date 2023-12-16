e20384c8ce52c66986f1046c764888822ec78476
JamesUdy <udayamvad@gmail.com>
2023-12-16 18:31:43 +0530

    feat: display 4 tasks for small devices

--- src/app/todo-form/toDoForm/EarlyStageTasks.tsx
+++ src/app/todo-form/toDoForm/EarlyStageTasks.tsx
@@ -3,25 +3,30 @@ import { ListProps } from '@/app/fetchToDoData/FetchToDoData';
 import TaskCard from '@/app/taskCard/TaskCard';
 import TaskLoader from '@/app/loader/taskLoader/TaskLoader';
 import Link from 'next/link';
-
+import { useMediaQuery } from 'react-responsive';
+ 
 interface EarlyStageTasksProps {
   taskData: ListProps[];
 };
 
 const EarlyStageTasks: React.FC<EarlyStageTasksProps> = ({taskData}) => {
 
+  const isLargerScreen = useMediaQuery({
+    minWidth: 1440,
+  });
+
   const earlyStageTasks = taskData.filter(
     task => task.taskProgress !== "Completed ✅"
   );
 
-  const firstThreeTasks = earlyStageTasks.slice(0, 3);
+  const displayTasks = isLargerScreen ? earlyStageTasks.slice(0, 3) : earlyStageTasks.slice(0, 4);
 
   return (
     <section className='w-full px-2 container flex flex-col space-y-8 items-center my-16 todo-list text-slate-700 dark:text-slate-400'>
         <span className='text-md w360:text-lg sm:text-xl font-semibold'>🌱 In the Pipeline: Early Stage Tasks</span>
-        {firstThreeTasks ? (
-          <section className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 sm:w-2/3 xl:w-1/2 gap-6 text-slate-900 dark:text-slate-400'>
-          {firstThreeTasks.map((task) => (
+        {displayTasks ? (
+          <section className='sm:columns-2 xl:columns-3 w-full sm:w-2/3 xl:w-1/2 gap-x-4 space-y-6 text-slate-900 dark:text-slate-400'>
+          {displayTasks.map((task) => (
             <TaskCard key={task.id} task={task} />
           ))}
           </section>
