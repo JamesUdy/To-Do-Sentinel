9c9b29ea7ee61b235f14866075fce3a3fb978056
JamesUdy <udayamvad@gmail.com>
2023-12-17 18:38:54 +0530

    refactor: extract website header into separate component

--- src/app/HomePage.tsx
+++ src/app/HomePage.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React, { useState } from 'react';
+import Header from './homepage/Header';
 import SignIn from './signIn/SignIn';
 import Navbar from './navbar/Navbar';
 import Profile from './profile/Profile';
@@ -28,11 +29,13 @@ const HomePage = () => {
         user ? (
           <>
             <Navbar handleShowProfile={handleShowProfile} userDp={user.photoURL}/>
+            <Header/>
             <SignIn/>
             <Profile handleShowProfile={handleShowProfile} showProfile={showProfile} userEmail={user.email} userEmailVerified={user.emailVerified} userName={user.displayName} userDp={user.photoURL} handleLogout={handleLogout}/>
           </>
         ) : (
           <>
+          <Header/>
           <SignIn/>
         </>
         )
--- src/app/signIn/SignIn.tsx
+++ src/app/signIn/SignIn.tsx
@@ -1,7 +1,7 @@
 
 import { auth } from "@/firebaseAuth/FirebaseAuth";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { DarkCurvedArrow, darkThemeLogo, LightCurvedArrow, lightThemeLogo } from "@/assets";
+import { DarkCurvedArrow, LightCurvedArrow } from "@/assets";
 import Image from "next/image";
 import "./SignIn.css";
 import { ThemeToggleButton } from '@/assets/todoAssets';
@@ -35,19 +35,7 @@ const SignIn = () => {
 
   return (
     <section className="flex flex-col items-center my-auto w-full space-y-4 font-mono">
-      <span className="text-3xl w360:text-5xl sm:text-6xl font-bold website-name">To Do Sentinel</span>
-      <p className="text-xs w360:text-md sm:text-lg text-center font-medium text-slate-800 dark:text-slate-500 my-4 px-4 sm:px-0 lg:w-1/2">
-        Welcome to To Do Sentinel - Your Ultimate Task Management App! 🚀✨<br />
-        Stay organized, boost productivity, and never forget a task 📈✅.
-        <br /><br />
-        Ready to take control of your tasks? 📋🏆
-        <br /><br />
-        Sign in now to start exploring your to-do list and supercharge your productivity! 🚀💪
-      </p>
-      <section className="overflow-hidden w-full mx-auto flex justify-center" aria-disabled='true'>
-        <Image loading="lazy" src={lightThemeLogo} alt="ToDo Sentinel Logo" className="rotate-logo w-1/2 sm:w-1/4 inline-block dark:hidden"/>
-        <Image loading="lazy" src={darkThemeLogo} alt="ToDo Sentinel Logo" className="rotate-logo w-1/2 sm:w-1/4 hidden dark:inline-block"/>
-      </section>
+      
       <section className="relative w-1/2 flex justify-center">
       { !loading ? (
           !isLoggedIn ? (
