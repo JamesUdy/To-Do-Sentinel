7ec85a8a06e4b66b0e32e5bd4eb656cf161ba8c2
JamesUdy <udayamvad@gmail.com>
2023-12-17 12:00:39 +0530

    feat: add metadata for user details page

--- src/app/user-details/page.tsx
+++ src/app/user-details/page.tsx
@@ -1,6 +1,26 @@
 
 import React from 'react';
 import DetailsPage from './DetailsPage';
+import { Metadata } from 'next';
+import SiteConstants from '@/constants/SiteConstants';
+
+export const generateMetadata = async (): Promise<Metadata> => {
+  const { devBaseURL, prodBaseURL, siteUserDetailsPageTitle, siteUserDetailsPageDescription } = SiteConstants();
+
+  let pageTitle = `${siteUserDetailsPageTitle}`;
+  let pageDescription = `${siteUserDetailsPageDescription}`;
+
+  let metadataBaseUrl: string = prodBaseURL ?? devBaseURL ?? '';
+
+  return {
+    metadataBase: new URL(metadataBaseUrl),
+    title: pageTitle,
+    openGraph: {
+      title: pageTitle,
+      description: pageDescription,
+    }
+  };
+};
 
 const UserDetails = () => {
   
--- src/constants/SiteConstants.tsx
+++ src/constants/SiteConstants.tsx
@@ -4,9 +4,13 @@ const SiteConstants = () => {
 
   const prodBaseURL = process.env.NEXT_PUBLIC_METADATA_PROD_BASE_URL;
   const devBaseURL = process.env.NEXT_PUBLIC_METADATA_DEV_BASE_URL  ;
+
   const siteName = "ToDo Sentinel";
   const siteCatchPhrase = "Your perfect 'to-do list' partner!";
-  const siteDescription = "Discover productivity like never before with ToDo Sentinel – your go-to to-do list platform! Navigate your tasks with ease, set priorities, and stay on top of your goals effortlessly. ToDo Sentinel offers a sleek and intuitive interface to elevate your task management experience. Maximize your efficiency, track progress, and conquer your to-do list with confidence. Join ToDo Sentinel now and take control of your productivity journey!";
+  const siteDescription = "Discover productivity like never before with ToDo Sentinel - your go-to to-do list platform! Navigate your tasks with ease, set priorities, and stay on top of your goals effortlessly. ToDo Sentinel offers a sleek and intuitive interface to elevate your task management experience. Maximize your efficiency, track progress, and conquer your to-do list with confidence. Join ToDo Sentinel now and take control of your productivity journey!";
+
+  const siteUserDetailsPageTitle = "User Details";
+  const siteUserDetailsPageDescription = "Your ToDo Sentinel User Details page - your command center for a personalized experience. Track your journey with joined date and last login details, while ensuring security with verified email status. Easily manage your profile information, including your name, email, and profile image. Stay connected, stay informed - it's all about you at ToDo Sentinel.";
 
   return {
     devBaseURL,
@@ -14,6 +18,9 @@ const SiteConstants = () => {
     siteCatchPhrase,
     siteDescription,
     siteName,
+    
+    siteUserDetailsPageTitle,
+    siteUserDetailsPageDescription,
   };
 };
 
