37dae84077e4b5b754fd968f4184094fc40faa25
JamesUdy <udayamvad@gmail.com>
2023-12-22 11:09:21 +0530

    [wip] ui: style home page scrollbar

--- src/app/HomePage.tsx
+++ src/app/HomePage.tsx
@@ -12,7 +12,7 @@ import { ThemeToggleButton } from '@/assets/todoAssets';
 
 const HomePage = () => {
   const [showProfile, setShowProfile] = useState(false);
-  const {router, setUser, user} = Constants();
+  const {router, scrollBar, setUser, user} = Constants();
   
   const handleShowProfile = () => {
     setShowProfile(!showProfile);
@@ -25,21 +25,23 @@ const HomePage = () => {
   };
 
   return (
-    <section className='relative flex flex-col w-full py-2 px-4 sm:px-1'>
+    <section className='relative flex flex-col w-full py-2 px-4 sm:px-1 max-h-screen overflow-y-hidden'>
       <Navbar handleShowProfile={handleShowProfile} userDp={user?.photoURL}/>
-      <Hero/>
-      <SignIn/>
-      {user && (
-        <Profile
-          handleShowProfile={handleShowProfile}
-          showProfile={showProfile}
-          userEmail={user.email}
-          userEmailVerified={user.emailVerified}
-          userName={user.displayName}
-          userDp={user.photoURL}
-          handleLogout={handleLogout}
-        />
-      )}
+      <div className={`overflow-y-scroll ${scrollBar}`}>
+        <Hero/>
+        <SignIn/>
+        {user && (
+          <Profile
+            handleShowProfile={handleShowProfile}
+            showProfile={showProfile}
+            userEmail={user.email}
+            userEmailVerified={user.emailVerified}
+            userName={user.displayName}
+            userDp={user.photoURL}
+            handleLogout={handleLogout}
+          />
+        )}
+      </div>
       {!user && <ThemeToggleButton />}
     </section>
   );
