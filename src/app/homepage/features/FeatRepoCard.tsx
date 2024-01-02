f0f02ed83d934ed281b455c2a50557b52c49039a
JamesUdy <udayamvad@gmail.com>
2024-01-02 23:12:09 +0530

    feat!!: remove demo task card content

--- src/app/homepage/features/FeatRepoCard.tsx
+++ src/app/homepage/features/FeatRepoCard.tsx
@@ -5,16 +5,12 @@ import Constants from '@/constants/Constants';
 import { FeatRepoTaskProps } from './FeatRepositories';
 
 const FeatRepoCard: React.FC<{task: FeatRepoTaskProps}> = ({ task }) => {
-    const { theme } = Constants();
-
-    const category = categories.find(category => category.label === task.taskPriority);
-    const backgroundColor = theme === 'dark' ? category?.colors.dark : category?.colors.light;
 
   return (
     <section className={`${task.id === 0 ? 'rotate-3' : '-rotate-6'} w-1/3 break-inside-avoid-column bg-white dark:bg-slate-900 p-2 flex flex-col justify-between h-full space-y-2 rounded-lg shadow-lg shadow-slate-600 dark:shadow-black border-1.5 border-slate-950`}>
         <div className='flex flex-col space-y-2'>
             <div className='flex justify-between items-center py-2'>
-                <span className={`${backgroundColor} ml-1 px-2 rounded-md text-sm text-white shadow-md shadow-slate-700 dark:shadow-slate-950`}>{task.taskPriority}</span>
+                <span className='ml-1 px-2 rounded-md bg-slate-800/70 h-3 w-16 animate-pulse shadow-md shadow-slate-700 dark:shadow-slate-950'></span>
                 <div className='flex space-x-2'>
                     <span className='cursor-pointer'>
                         <Edit/>
@@ -26,17 +22,16 @@ const FeatRepoCard: React.FC<{task: FeatRepoTaskProps}> = ({ task }) => {
             </div>
             <div className='px-2 flex flex-col space-y-1'>
                 <div className='flex space-x-2 items-center'>
-                    <span className={`text-md sm:text-lg font-semibold ${task.taskProgress === 'Completed ✅' ? 'line-through' : ''}`}>{task.taskTitle}</span>
-                    <span className={`${task.taskProgress === 'In Progress 🚧' ? 'bg-orange-600 animate-pulse' : task.taskProgress === 'Completed ✅' ? 'bg-green-600 animate-pulse' : 'bg-gray-600 dark:bg-gray-300 animate-bounce'} w-4 h-4 px-2 rounded-full`}></span>
+                    <span className='h-3 bg-slate-800/70 rounded-md w-1/2 animate-pulse shadow-md shadow-slate-700 dark:shadow-slate-950'></span>
+                    <span className='bg-gray-600 dark:bg-slate-800/70 w-4 h-4 px-2 rounded-full animate-pulse shadow-md shadow-slate-700 dark:shadow-slate-950'></span>
                 </div>
-                <span className='text-xs sm:text-sm text-start font-normal'>{task.taskDescription}</span>
+                <span className='h-3 w-full bg-slate-800/70 rounded-md animate-pulse shadow-md shadow-slate-700 dark:shadow-slate-950'></span>
+                <span className='h-3 w-5/6 bg-slate-800/70 rounded-md animate-pulse shadow-md shadow-slate-700 dark:shadow-slate-950'></span>
             </div>
         </div>
         <div className='flex w-full items-center justify-between pt-4 pb-2 px-2 sm:px-0 sm:pl-2'>
-            {task.taskDueDate && (
-                <span className={`${task.id === 0 ? 'bg-red-700' : 'bg-red-400'} animate-pulse text-white px-2 py-0.5 text-sm rounded-md w-fit`}>Due</span>
-            )}
-            <span className='text-end w-full text-2xs font-medium text-slate-500'>{task.createdAt}</span>
+            <span className='h-3 w-8 bg-slate-800/70 rounded-md animate-pulse shadow-md shadow-slate-700 dark:shadow-slate-950'></span>
+            <span className='h-3 w-16 bg-slate-800/70 rounded-md animate-pulse shadow-md shadow-slate-700 dark:shadow-slate-950'></span>
         </div>
     </section>
   );
