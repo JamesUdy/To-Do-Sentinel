f623df593687aad46135cd5f21983f8083f22efb
JamesUdy <udayamvad@gmail.com>
2024-01-06 08:57:43 +0530

    [wip] feat: add FeatCustom component into features

--- src/app/homepage/Features.tsx
+++ src/app/homepage/Features.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import FeatForm from './features/FeatForm';
 import FeatRepositories from './features/FeatRepositories';
+import FeatCustom from './features/FeatCustom';
 
 const Features = () => {
   return (
     <div className='flex flex-col items-center w-full max-w-7xl p-4 space-y-20'>
         <FeatForm/>
         <FeatRepositories/>
+        <FeatCustom/>
     </div>
   );
 };
