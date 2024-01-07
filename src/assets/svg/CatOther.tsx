5e1383e1b562467f74f76bfa29b8199e37511ca3
JamesUdy <udayamvad@gmail.com>
2024-01-07 20:14:00 +0530

    docs: create `CatOther` component

---
+++ src/assets/svg/CatOther.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CatOther = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+        <path fill="currentColor" fillRule="evenodd" d="M17 3a4 4 0 1 0 0 8a4 4 0 0 0 0-8M3 17a4 4 0 1 1 8 0a4 4 0 0 1-8 0m10-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v5a2 2 0 0 1-2 2h-4a2 2 0 0 1-2-2zM3 4a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v5a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" clipRule="evenodd">
+        </path>
+    </svg>
+  );
+};
+
+export default CatOther;
