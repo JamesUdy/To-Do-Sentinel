209695e16c25fd06eeb28db8e0287eac0a4f3c0a
JamesUdy <udayamvad@gmail.com>
2024-01-07 20:24:48 +0530

    feat: create a `CategoryData` array

---
+++ src/app/homepage/features/CustomiseData/CategoryData.ts
@@ -0,0 +1,34 @@
+import { CatBooks, CatBusiness, CatFitness, CatHome, CatOther, CatPersonal } from "@/assets";
+
+export const categoryData = [
+    {
+        id: 0,
+        icon: CatPersonal,
+        label: 'Personal',
+    },
+    {
+        id: 1,
+        icon: CatHome,
+        label: 'Home',
+    },
+    {
+        id: 2,
+        icon: CatBooks,
+        label: 'Studies',
+    },
+    {
+        id: 3,
+        icon: CatBusiness,
+        label: 'Business',
+    },
+    {
+        id: 4,
+        icon: CatFitness,
+        label: 'Fitness',
+    },
+    {
+        id: 5,
+        icon: CatOther,
+        label: 'Other',
+    },
+];
