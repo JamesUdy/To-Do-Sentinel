f93f968b4d5f787b675649384b453325f9168863
JamesUdy <udayamvad@gmail.com>
2024-01-07 20:14:19 +0530

    docs: create `CatPersonal` component

---
+++ src/assets/svg/CatPersonal.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CatPersonal = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+        <path fill="currentColor" d="M12 4a3.5 3.5 0 1 0 0 7a3.5 3.5 0 0 0 0-7M6.5 7.5a5.5 5.5 0 1 1 11 0a5.5 5.5 0 0 1-11 0M3 19a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v3H3zm5-3a3 3 0 0 0-3 3v1h5.613l-1.334-4zm3.387 0L12 17.838L12.613 16zm3.334 0l-1.334 4H19v-1a3 3 0 0 0-3-3z">
+        </path>
+    </svg>
+  );
+};
+
+export default CatPersonal;
