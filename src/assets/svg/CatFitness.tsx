88146481abc3a050edf1fb67a7cf3b1dce8987d5
JamesUdy <udayamvad@gmail.com>
2024-01-08 11:42:51 +0530

    ui: adjust size for category icon

--- src/assets/svg/CatFitness.tsx
+++ src/assets/svg/CatFitness.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const CatFitness = () => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512">
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512" className='h-12 w-12'>
         <path fill="currentColor" d="M480 272h-37.34a261.41 261.41 0 0 1-18.25 32H480ZM32 240v32h37.34a225.1 225.1 0 0 1-12.4-32Z">
         </path>
         <path fill="currentColor" d="m304 259.78l-51.73 103.46l-48-160L169.89 272H69.34c10 20.92 23.5 41.41 40.63 61.68c40.12 47.46 94.25 79.75 137 108.32l9 6l9-6c42.78-28.57 96.91-60.86 137-108.32A322.78 322.78 0 0 0 424.41 304h-98.3Z">
