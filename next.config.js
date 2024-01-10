b48f8ab55536415695417cc255aec9293e30ee64
JamesUdy <udayamvad@gmail.com>
2024-01-10 12:57:17 +0530

    feat: incorporate pwa in nextConfig

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        domains: ['lh3.googleusercontent.com'],
         remotePatterns: [
           {
             protocol: 'https',
@@ -11,4 +12,16 @@ const nextConfig = {
       },
 }
 
-module.exports = nextConfig
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
+module.exports = withPWA({...nextConfig});
