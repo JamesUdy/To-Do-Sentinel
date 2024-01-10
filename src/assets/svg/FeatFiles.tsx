39c42dbef4863cf8feb1e49b5223d9939a422aed
JamesUdy <udayamvad@gmail.com>
2024-01-10 11:17:11 +0530

    docs: create `FeatFiles` component

---
+++ src/assets/svg/FeatFiles.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FeatFiles = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16" className='w-10 h-10'>
+        <path fill="currentColor" fillRule="evenodd" d="M8 10.5h4A1.5 1.5 0 0 0 13.5 9V7H12a3 3 0 0 1-3-3V2.5H8A1.5 1.5 0 0 0 6.5 4v5A1.5 1.5 0 0 0 8 10.5m5.06-5.318c.096.096.178.203.243.318H12A1.5 1.5 0 0 1 10.5 4V2.697c.115.065.223.147.318.242zM15 6.242a3 3 0 0 0-.879-2.12L11.88 1.878A3 3 0 0 0 9.757 1H8a3 3 0 0 0-3 3H4a3 3 0 0 0-3 3v5a3 3 0 0 0 3 3h4a3 3 0 0 0 3-3h1a3 3 0 0 0 3-3V6.243ZM9.5 12H8a3 3 0 0 1-3-3V5.5H4A1.5 1.5 0 0 0 2.5 7v5A1.5 1.5 0 0 0 4 13.5h4A1.5 1.5 0 0 0 9.5 12" clipRule="evenodd">
+        </path>
+    </svg>
+  );
+};
+
+export default FeatFiles;
