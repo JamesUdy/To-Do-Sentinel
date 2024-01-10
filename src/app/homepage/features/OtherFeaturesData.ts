7f00fb97f051924e45a9d3ad782c8f3b8fbbf546
JamesUdy <udayamvad@gmail.com>
2024-01-10 11:19:44 +0530

    copy: create array object for `OtherFeature` component

---
+++ src/app/homepage/features/OtherFeaturesData.ts
@@ -0,0 +1,28 @@
+import { FeatAuthentication, FeatFiles, FeatNotification, FeatTheme } from "@/assets";
+
+export const otherData = [
+    {
+        id: 0,
+        icon: FeatAuthentication,
+        title: 'End-to-End Encryption',
+        description: 'Ensure the safety of your task data through secure authentication with FireStore, enhancing the overall integrity of your to-do list management system.',
+    },
+    {
+        id: 1,
+        icon: FeatNotification,
+        title: 'Deadline Alerts',
+        description: 'Stay on track by receiving timely push notifications for approaching and overdue tasks, ensuring nothing falls through the cracks.',
+    },
+    {
+        id: 2,
+        icon: FeatTheme,
+        title: 'Dual Theme Support',
+        description: 'Personalize your to-do list environment with dual theme options, providing a comfortable and visually appealing experience.',
+    },
+    {
+        id: 3,
+        icon: FeatFiles,
+        title: 'Efficient File Handling',
+        description: 'Streamline task organization with the ability to attach and manage multiple files for each to-do item.',
+    },
+];
