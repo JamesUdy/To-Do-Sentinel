82fa0d4391827b986b94af04f98afbb9ca4c783d
JamesUdy <udayamvad@gmail.com>
2024-01-12 12:11:04 +0530

    ui: change text icon color in `FeatCustom` component

--- src/app/homepage/features/FeatCustom.tsx
+++ src/app/homepage/features/FeatCustom.tsx
@@ -52,7 +52,7 @@ const FeatCustom = () => {
   return (
     <section className='flex flex-col space-y-8 sm:space-y-0 w-full font-medium'>
             <div className='flex flex-col w-full lg:w-4/5 space-y-4'>
-                <div className='bg-pink-400/80 dark:bg-pink-600 border-3 dark:border-2 border-pink-500 w-fit p-2 rounded-full'>
+                <div className='bg-pink-300 dark:bg-pink-600 border-3 dark:border-2 border-pink-400 text-pink-600 dark:text-slate-100 w-fit p-2 rounded-full'>
                     <TaskCustom/>
                 </div>
                 <div className='flex flex-col space-y-2'>
