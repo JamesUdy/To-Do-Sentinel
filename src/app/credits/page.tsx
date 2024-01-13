ba8b028fba70bce17980b04c13f618d2f3e99575
JamesUdy <udayamvad@gmail.com>
2024-01-13 20:26:07 +0530

    docs: add metadata from `SiteConstants` to credits page

--- src/app/credits/page.tsx
+++ src/app/credits/page.tsx
@@ -1,4 +1,24 @@
 import React from 'react';
+import { Metadata } from 'next';
+import SiteConstants from '@/constants/SiteConstants';
+
+export const generateMetadata = async (): Promise<Metadata> => {
+    const { devBaseURL, prodBaseURL, siteName, siteCreditsPageTitle, siteCreditsPageDescription } = SiteConstants();
+  
+    let pageTitle = `${siteName} / ${siteCreditsPageTitle}`;
+    let pageDescription = `${siteCreditsPageDescription}`;
+  
+    let metadataBaseUrl: string = prodBaseURL ?? devBaseURL ?? '';
+  
+    return {
+      metadataBase: new URL(metadataBaseUrl),
+      title: pageTitle,
+      openGraph: {
+        title: pageTitle,
+        description: pageDescription,
+      }
+    };
+  };
 
 const CreditsPage = () => {
   return (
