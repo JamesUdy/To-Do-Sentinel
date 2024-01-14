3a27902a9d1c6d3db6e8c3515ea169eec0b9f821
JamesUdy <udayamvad@gmail.com>
2024-01-14 21:56:42 +0530

    ui: make content in the center of the page

--- src/app/credits/Credits.tsx
+++ src/app/credits/Credits.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Credits = () => {
   return (
-    <div>Credits</div>
+    <div  className='flex justify-center items-center w-full h-full'>Credits</div>
   );
 };
 
