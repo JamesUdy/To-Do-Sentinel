8f5fb3e4ce06fb7b5a916ceae41419e5bf1cbb43
JamesUdy <udayamvad@gmail.com>
2024-03-01 21:05:41 +0530

    docs: create and add `gettingStarted` data

---
+++ src/app/homepage/SummaryData.ts
@@ -0,0 +1,27 @@
+export const gettingStarted = [
+    {
+        id: 1,
+        name: 'heroContent',
+        label: 'Introduction'
+    },
+    {
+        id: 2,
+        name: 'formContent',
+        label: 'Demo Form'
+    },
+    {
+        id: 3,
+        name: 'repositoryContent',
+        label: 'Repository'
+    },
+    {
+        id: 4,
+        name: 'customContent',
+        label: 'Editing'
+    },
+    {
+        id: 5,
+        name: 'extraFeaturesContent',
+        label: 'Other Features' 
+    },
+];
