80f77beb9d69fae6da33849027b66a1912761399
JamesUdy <udayamvad@gmail.com>
2024-03-02 20:25:56 +0530

    refactor: pass all the summary data to `summaryContainer` to make it dynamic

--- src/app/homepage/Summary.tsx
+++ src/app/homepage/Summary.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import Link from 'next/link';
 import Image from "next/image";
 import { darkThemeLogo, lightThemeLogo } from '@/assets';
-import { gettingStarted } from './SummaryData';
+import { contact, gettingStarted, pages } from './SummaryData';
+import SummaryContainer from './SummaryContainer';
+import Constants from '@/constants/Constants';
 
 const Summary = () => {
+
+    const { isLoggedIn } = Constants();
 
   return (
     <section className='px-16 flex space-x-16'>
@@ -18,23 +22,10 @@ const Summary = () => {
             </Link>
             <p className='font-normal text-justify text-slate-600'>Experience unparalleled productivity with ToDo Sentinel! Seamlessly manage tasks, set priorities, and achieve your goals effortlessly. With its sleek interface, tracking progress and staying organized has never been easier. Join ToDo Sentinel today and conquer your to-do list with confidence!</p>
         </div>
-        <div className='w-2/3'>
-            <div>
-                <span className='font-bold text-md'>Getting Started</span>
-                <ul className='space-y-2 pt-4 font-medium'>
-                    {gettingStarted.map((item) => (
-                        <li key={item.id} className='text-slate-600 hover:text-slate-950 dark:text-slate-500 dark:hover:text-slate-200'>
-                            <a href={`#${item.name}`}>{item.label}</a>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-            <div>
-                <ul></ul>
-            </div>
-            <div>
-                <ul></ul>
-            </div>
+        <div className='w-2/3 flex justify-center space-x-24'>
+            <SummaryContainer title="Getting Started" listData={gettingStarted} />
+            {isLoggedIn && <SummaryContainer title="Pages" listData={pages} />}
+            <SummaryContainer title="Contact Us" listData={contact} />
         </div>
     </section>
   );
