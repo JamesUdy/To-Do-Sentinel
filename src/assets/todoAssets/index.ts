838e9105db95565ca0764d886a84d74db7c3b9ee
JamesUdy <udayamvad@gmail.com>
2024-03-03 18:40:23 +0530

    copy: import it into `todoAssets` file

--- src/assets/todoAssets/index.ts
+++ src/assets/todoAssets/index.ts
@@ -4,9 +4,11 @@ import Navbar from '@/app/navbar/Navbar';
 import Profile from '@/app/profile/Profile';
 import ThemeToggleButton from "@/app/ThemeToggleButton";
 import ToDoForm from '@/app/todo-form/toDoForm/ToDoForm';
+import Footer from '@/app/Footer/Footer';
 
 export {
   EarlyStageTasks,
+  Footer,
   Loader,
   Navbar,
   Profile,
