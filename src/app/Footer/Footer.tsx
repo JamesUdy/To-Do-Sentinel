21d8031a3d8bf7c153138cf8850870cab79dba7c
JamesUdy <udayamvad@gmail.com>
2024-03-03 18:39:51 +0530

    docs: create a footer component

---
+++ src/app/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className='text-sm text-slate-500 dark:text-slate-700 w-full flex justify-center pt-10 footer'>
+        <p>&copy; 2024 To Do Sentinel. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
